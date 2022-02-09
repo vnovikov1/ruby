c33ad7310beb5d77718dde53ae0af7200b01bc27
vnovikov1 <63616307+vnovikov1@users.noreply.github.com>
2022-02-09 18:10:58 +0300

    lab1 task2-3 case

--- lr1/task2.rb
+++ lr1/task2.rb
@@ -2,8 +2,13 @@ puts "Привет, #{ARGV[0]}"
 puts "Какой твой любимый ЯП?"
 language = STDIN.gets.chomp
 
-unless language == "ruby" 
-    puts "Скоро будет Ruby!"
-else
+case language
+when "ruby"
     puts "Ты подлиза!"
+when "python"
+    puts "Скоро будет Ruby!"
+when "c++" 
+    puts "Здоровья."
+else
+    puts "Я не знаю такого :("
 end
