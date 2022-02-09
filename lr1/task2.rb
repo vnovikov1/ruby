93a41f78b1eb758a1dd4116238506d2b7f60cc94
vnovikov1 <63616307+vnovikov1@users.noreply.github.com>
2022-02-09 18:00:09 +0300

    lab1 task2-3 way4

--- lr1/task2.rb
+++ lr1/task2.rb
@@ -2,5 +2,8 @@ puts "Привет, #{ARGV[0]}"
 puts "Какой твой любимый ЯП?"
 language = STDIN.gets.chomp
 
-puts "Ты подлиза!" if language == "ruby"
-puts "Скоро будет Ruby!" if language != "ruby"
+unless language == "ruby" 
+    puts "Скоро будет Ruby!"
+else
+    puts "Ты подлиза!"
+end
