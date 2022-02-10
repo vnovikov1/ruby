fe82425337ac7f0b234c5eb76117552087cdfec9
vnovikov1 <63616307+vnovikov1@users.noreply.github.com>
2022-02-10 17:12:51 +0300

    lab1 task2.3 method3

--- lr1/task2.3.rb
+++ lr1/task2.3.rb
@@ -26,5 +26,52 @@ def method2(num)
     end
 end
 
+def sum_of_digits(num)
+    sum = 0
+    while num != 0 do
+        a = num % 10
+        sum += a
+        num /= 10
+    end
+    return sum
+end
+
+def product_of_digits(num)
+    pr = 1 
+    while num != 0 do
+        a = num % 10
+        pr *= a
+        a = num /= 10
+    end
+    return pr
+end
+
+def coprime?(num1, num2)
+    if num1 > num2
+        num1, num2 = num2, num1
+    end
+    for i in 2..num2
+        if num1 % i == 0 && num2 % i == 0
+            return false
+        end
+    end
+    return true
+end
+
+def method3(num)
+    sum = 0
+    for i in 1..num
+        if num % i == 0
+            if coprime?(i, sum_of_digits(num))
+                if coprime?(i, product_of_digits(num)) == false
+                    sum += i
+                end
+            end
+        end
+    end
+    return sum
+end
+
 puts method1(ARGV[0].to_i)
 puts method2(ARGV[0].to_i)
+puts method3(ARGV[0].to_i)
