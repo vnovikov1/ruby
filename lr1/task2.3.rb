739ed1d4c4af38eec5e94da4cacc39ec64eaa898
vnovikov1 <63616307+vnovikov1@users.noreply.github.com>
2022-02-10 16:51:17 +0300

    lab1 task2.3 method2

--- lr1/task2.3.rb
+++ lr1/task2.3.rb
@@ -8,4 +8,23 @@ def method1(num)
     return amount
 end
 
+def method2(num)
+    min = num % 10
+    num /= 10
+    if min == 0 then min = num % 10 end
+    while num != 0 do
+        a = num % 10
+        if a % 2 != 0 && a < min
+            min = a
+        end
+        num /= 10
+    end
+    if min % 2 == 0 
+        puts "Нечетных цифр нет!"
+    else
+        return min
+    end
+end
+
 puts method1(ARGV[0].to_i)
+puts method2(ARGV[0].to_i)
