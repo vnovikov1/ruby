68a6d09f4ce10ae915835dace7da970548ce600b
vnovikov1 <63616307+vnovikov1@users.noreply.github.com>
2022-03-02 14:08:20 +0300

    lab1 task5 method1.14

--- lr1/task5.rb
+++ lr1/task5.rb
@@ -6,11 +6,16 @@ def is_palindrome?(str)
     return str == str.reverse 
 end
 
+def sort_by_number(str)
+    return str.split.sort_by(&:size).join(' ')
+end
+
 def main()
     p "Введите строку: "
     str = gets.chomp
     p rand_str(str)
     p is_palindrome?(str.scan((/[A-Z]/)))
+    p sort_by_number(str)
 end
 
 main()
