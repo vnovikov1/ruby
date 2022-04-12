111696adc8927ecaa88a2c7bf2e844f2a7543df8
vnovikov1 <63616307+vnovikov1@users.noreply.github.com>
2022-04-13 00:15:35 +0300

    task5 cyrillic encode + date finder

--- lr1/task5.rb
+++ lr1/task5.rb
@@ -1,3 +1,14 @@
+def cyrillic
+    if (Gem.win_platform?)
+        Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+        Encoding.default_internal = __ENCODING__
+      
+        [STDIN, STDOUT].each do |io|
+          io.set_encoding(Encoding.default_external, Encoding.default_internal)
+        end
+    end
+end
+
 def rand_str(str)
     return str.split("").sort_by{rand}.join
 end
@@ -10,12 +21,18 @@ def sort_by_number(str)
     return str.split.sort_by(&:size).join(' ')
 end
 
+def date(str)
+    str.scan(/([0-2][0-9]|3[0-1]).(0[1-9]|1[0-2]|января|февраля|марта|апреля|мая|июня|июля|августа|сентября|октября|ноября|декабря).([0-9]{4})/).map{|e| e.join(' ')}.join(" ")
+end
+
 def main()
     p "Введите строку: "
     str = gets.chomp
     p rand_str(str)
     p is_palindrome?(str.scan((/[A-Z]/)))
     p sort_by_number(str)
+    p date(str)
 end
 
+cyrillic()
 main()
