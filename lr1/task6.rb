59a51150e5f005c2527e719edf1748f66546e36c
vnovikov1 <63616307+vnovikov1@users.noreply.github.com>
2022-05-02 20:50:15 +0300

    task6 method1.5 upd

--- lr1/task6.rb
+++ lr1/task6.rb
@@ -1,11 +1,40 @@
+def cyrillic
+    if (Gem.win_platform?)
+        Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+        Encoding.default_internal = __ENCODING__
+      
+        [STDIN, STDOUT].each do |io|
+          io.set_encoding(Encoding.default_external, Encoding.default_internal)
+        end
+    end
+end
+
 def min_number(str)
     return str.scan(%r"[0-9]*").uniq.delete_if{|x| x == ""}.map{|x| x.to_i}.min
+end
+
+def cyrillic_counter(str)
+    results = []
+    acc = 0
+    str.each_char do |c|
+        if c.ord >= 1040 && c.ord <= 1103
+            acc += 1
+        else
+            results.append(acc)
+            acc = 0
+        end
+    end
+    results.append(acc)
+    results.max
 end
 
 def main
     p "Введите строку: "
     str = gets.chomp
     p min_number(str)
+    p cyrillic_counter(str)
 end
 
+cyrillic()
 main()
+
