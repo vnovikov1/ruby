d0f5f2c629e469e791ecb98a552bc063142d4dfb
vnovikov1 <63616307+vnovikov1@users.noreply.github.com>
2022-05-04 10:55:06 +0300

    lab2 task1.2

--- lr2/lab2.rb
+++ lr2/lab2.rb
@@ -5,3 +5,8 @@ class Department
         @phone = phone
     end
 end
+
+dep_marketing = Department.new("Отдел маркетинга","89005002020")
+dep_it = Department.new("IT-отдел","89002009243")
+p "Подразделение - #{dep_marketing.name}, номер - #{dep_marketing.phone}"
+p "Подразделение - #{dep_it.name}, номер - #{dep_it.phone}"
