0bafa51bc7a9824300d241384377c7ff381e5ad6
vnovikov1 <63616307+vnovikov1@users.noreply.github.com>
2022-05-25 10:51:13 +0300

    lr2 main corrected

--- lr2/main.rb
+++ lr2/main.rb
@@ -3,8 +3,8 @@ require './department.rb'
 def read_from_txt(path)
     arr = Array.new
     File.open("#{path}").each do |dep| 
-        name, phone_number = dep.chomp.split(",")
-        arr.append(Department.new(name, phone_number))
+        name, phone = dep.chomp.split(",")
+        arr.append(Department.new(name, phone))
     end
     arr
 end
@@ -23,3 +23,9 @@ def print_arr(arr)
         puts obj
     end
 end
+
+# arr = []
+# arr.append(Department.new("Отдел маркетинга","89005002020"))
+# write_to_txt("deps.txt",arr)
+# # arr = read_from_txt("deps.txt")
+# # print_arr(arr)
